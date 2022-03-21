727714c7965c860279b626467367af1c107c7a96
Pimenta <mateuspimartins@hotmail.com>
2022-03-21 17:10:55 -0300

    final version i'm crying :'(

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,31 +1,70 @@
-// import { useState } from "react"
-import Card from './Card';
+import { useState } from "react"
+// import Card from './Card';
 
-export default function Cards({ deck, updateNumCompleted, updateStatusIcon, updateZapAmount, reestart, updateReestart }) {
-    return (
-        <div className="deck">
-            {deck.map(({ question, answer }, questionNum) => {
-                return (
-                    <Flashcard
-                        questionNum={questionNum + 1}
-                        key={questionNum + 1}
-                        question={question}
-                        answer={answer}
-                        updateNumCompleted={updateNumCompleted}
-                        updateStatusIcon={updateStatusIcon}
-                        updateZapAmount={updateZapAmount}
-                        reestart={reestart}
-                        updateReestart={updateReestart}
-                    />
-                )
-            })
-            }
+export default function Cards() {
+
+    const deck = [{
+        question: "O que é JSX?",
+        answer: "Uma extensão de linguagem do JavaScript"
+    }, {
+        question: "O React é __",
+        answer: "Uma biblioteca JavaScript para construção de interfaces"
+    }, {
+        question: "Componentes devem iniciar com __",
+        answer: "Letra maiúscula"
+    }, {
+        question: "Podemos colocar __ dentro do JSX",
+        answer: "Expressões"
+    }, {
+        question: "O ReactDOM nos ajuda __ ",
+        answer: "Interagindo com a DOM para colocar componentes React na mesma"
+    }, {
+        question: "Usamos o npm para __",
+        answer: "Gerenciar os pacotes necessários e suas dependências"
+    }, {
+        question: "Usamos props para __",
+        answer: "Passar diferentes informações para componentes"
+    }, {
+        question: "Usamos estado (state) para __",
+        answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+    }
+    ]
+
+    const [aberto, setAberto] = useState(false)
+    
+    // function qStat(status){
+    //     if(questStatus === "init" && status === "not-init"){
+    //         setQuestStatus("not-init")
+    //     }else{
+    //         setQuestStatus("init")
+    //     }
+    // }
+
+    let classe = 'question-card'
+    let src = "./assets/play.png"
+    
+    if(aberto){
+        classe = 'opened-card'
+        src = './assets/setinha.png'
+    }
+    
+
+    function toggleCard(){
+        setAberto(!aberto)
+    }
+
+    
+    let deckmap = deck.map((quest, i) => 
+        <div key={i} className={classe} onClick={toggleCard}>
+            <p> {aberto ? quest.question : ("Pergunta " + (i+1)) } </p>
+            <img src={src} alt="img"/>
         </div>
     )
+    return (deckmap);
 }
 
 // return(
-// <div className="cards">sad
+// <div className="cards">
 //     <div className="question-card">
 //       <p>Pergunta x</p>
 //       <img src="./assets/play.png" />
--- src/index.js
+++ src/index.js
@@ -5,18 +5,25 @@ import InitialPage from './components/InitialPage';
 import Header from './components/Header';
 import Cards from './components/Cards';
 
+
+
 function App() {
     const [pageStatus, setPageStatus] = useState("initial")
+
 
     return (
         <>
             {pageStatus === "initial"
-                ? <InitialPage 
-                pageStatus={updatedPageStatus => setPageStatus(updatedPageStatus)}
+                ? <InitialPage
+                    pageStatus={updatedPageStatus => setPageStatus(updatedPageStatus)}
                 />
                 : <>
                     <Header />
+                    <>
+                    <div className="cards">
                     <Cards />
+                    </div>
+                    </>
                 </>
             }
         </>
